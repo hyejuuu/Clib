0f568e2e7cb60a1b246c4e90243eb6891370f158
hyeju <hyeju.tech@gmail.com>
2020-06-23 23:42:59 +0900

    add loginView

--- Clib/Clib/Scenes/Login/View/LoginMenuViewController.swift
+++ Clib/Clib/Scenes/Login/View/LoginMenuViewController.swift
@@ -53,7 +53,45 @@ class LoginMenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print(solution(3, [3], [1]))
+
         setupLayout()
+    }
+
+    func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+        var students: [Int] = Array(repeating: 1, count: n)
+        
+        for index in lost {
+            students[index-1] -= 1
+        }
+        
+        for index in reserve {
+            students[index-1] += 1
+        }
+        
+        for index in 0..<n {
+            if students[index] == 0 {
+                if index != 0 {
+                    if students[index - 1] > 1 {
+                        students[index] += 1
+                        students[index-1] -= 1
+                    }
+                } else if index != n - 1 {
+                    if students[index + 1] > 1 {
+                        students[index] += 1
+                        students[index+1] -= 1
+                    }
+                }
+            }
+        }
+        
+        var result = 0
+        
+        for element in students {
+            if element > 0 { result += 1 }
+        }
+        
+        return result
     }
 
     private func setupLayout() {
